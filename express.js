2a40d7ce981bb73c3533688781462f95ab06d084
MrPravinS <shegamwarpravin07@gmail.com>
2025-08-06 21:45:48 +0530

    new feature added

--- express.js
+++ express.js
@@ -1,1 +1,12 @@
+
 console.log("hello Words");
+
+
+
+function add(a,b){
+    console.log("New feature added");
+    
+    return a + b;
+}
+
+console.log(add(43,2));
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Devops Mastery
+
+- Start Version Controle Git and GitHub
+  - basics command
+   - git init : for initialize git
